50d6f8716b5eee2627ad4e88a9ef2a275f070a4d
Jan <jannemetguzvic@gmail.com>
2023-02-15 18:49:32 +0100

    updated webpack

--- 02-webpack/index.js
+++
@@ -1,4 +0,0 @@
-const nanoid = require("nanoid");
-
-console.log("Hello World!");
-console.log(nanoid.nanoid());
--- 02-webpack/webpack.config.js
+++ 02-webpack/webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 module.exports = {
     mode: "production",
-    entry: path.resolve(__dirname,"index.js"),
+    entry: path.resolve(__dirname,"src/index.js"),
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "dist/"),
